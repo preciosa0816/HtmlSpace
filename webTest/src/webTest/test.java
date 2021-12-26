248533ca2d5307f42faa6c9295feea16e67f3882
Yunji <preciosa0816@gmail.com>
2021-12-26 23:56:13 +0900

    html Study material

---
+++ webTest/src/webTest/test.java
@@ -0,0 +1,17 @@
+package webTest;
+
+import java.util.Scanner;
+
+public class test {
+	public static void main(String[] args) {
+		int A;
+		int B;
+
+		Scanner sc = new Scanner(System.in);
+		A= sc.nextInt();
+		B= sc.nextInt();
+		
+		
+		
+	}
+}
